d84240e5b8a657cd136c76e86501176d45641619
Veeruachar <veereshcse.rymec@gmail.com>
2025-11-23 01:35:26 +0530

    create table sql file added

---
+++ src/main/resources/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE driving_school_db.registrations_data (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255),
+    address VARCHAR(255),
+    email VARCHAR(255),
+    phone VARCHAR(255),
+    course_type VARCHAR(255),
+    dlnumber VARCHAR(255)
+);
